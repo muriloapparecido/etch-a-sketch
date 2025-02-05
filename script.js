b55a1dbb35ac28f5b8d3d7be5c35b93155ba2cc8
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-02-05 00:55:18 -0500

    Add Hover Effect to Change Color of Grid Items

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ container.style.width = "740px";
 container.style.margin = "0 auto"; 
 
 for (let i = 0; i < 256; i++){
-    const gridItem = document.createElement("div");
+    const gridItem = document.createElement("div"); 
     gridItem.style.width = "40px";
     gridItem.style.height = "40px";
-    gridItem.style.border = "2px solid black"; 
+    gridItem.style.border = "2px solid white"; 
+
+
+    //Changes color of gridItem when hovered
+    gridItem.addEventListener("mouseover", () => {
+        const randomColor = `rgb(${Math.floor(Math.random() * 256)}, 
+                                ${Math.floor(Math.random() * 256)}, 
+                                ${Math.floor(Math.random() * 256)})`;
+
+        gridItem.style.backgroundColor = randomColor;
+    })
+
     container.appendChild(gridItem); 
 }
+
