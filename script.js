955f23cd62cd971ab2bda092242e374a4ef765ce
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-02-04 23:42:39 -0500

    Complete 16X16 Grid

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const container = document.querySelector("#container"); 
+container.style.display = "flex"; 
+container.style.justifyContent = "center"; 
+container.style.flexWrap = "wrap"; 
+container.style.width = "740px";
+container.style.margin = "0 auto"; 
 
-
+for (let i = 0; i < 256; i++){
+    const gridItem = document.createElement("div");
+    gridItem.style.width = "40px";
+    gridItem.style.height = "40px";
+    gridItem.style.border = "2px solid black"; 
+    container.appendChild(gridItem); 
+}
