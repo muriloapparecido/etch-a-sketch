3dd6283d1504869fddda3cd9304f41b19dff1fd3
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-02-06 23:47:13 -0500

    Added Reset Grid Button and Styling

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const container = document.querySelector("#container"); 
-const button = document.querySelector("#resize-button"); 
+const buttonContainer = document.createElement("div"); 
+const resizeButton = document.querySelector("#resize-button"); 
+const resetButton = document.querySelector("#reset-button"); 
+
+//Styling
+buttonContainer.style.display = "flex"; 
+buttonContainer.style.justifyContent = "center"; 
+buttonContainer.style.gap = "10px"; 
+buttonContainer.style.width = "auto"; 
+buttonContainer.style.height = "800px"; 
+buttonContainer.style.marginLeft = "100px"; 
+buttonContainer.style.marginRight = "-300px"; 
+
+resizeButton.style.height = "50px";
+resizeButton.style.width = "100px";
+
+resetButton.style.height = "50px";
+resetButton.style.width = "100px";
 
 container.style.display = "flex"; 
 container.style.flexWrap = "wrap"; 
@@ -7,17 +24,22 @@ container.style.width = "800px";
 container.style.height = "800px"; 
 container.style.margin = "0 auto"; 
 
-button.style.marginLeft = "200px";
-button.style.height = "50px"
-button.style.width = "100px"
-button.style.position = "relative";
-button.style.top = "50px"; 
+const mainContainer = document.createElement("div");
+mainContainer.style.display = "flex"; 
+mainContainer.style.alignItems = "center"; 
+mainContainer.appendChild(buttonContainer);
+mainContainer.appendChild(container);
 
+buttonContainer.appendChild(resizeButton);
+buttonContainer.appendChild(resetButton);
+
+document.body.appendChild(mainContainer);
 
 function createGrid(size) {
     container.innerHTML = ""; 
     const squareSize = 800 / size 
 
+    //Create each grid square
     for (let i = 0; i < size * size; i++){
         const gridItem = document.createElement("div"); 
         gridItem.style.width = `${squareSize}px`;
@@ -39,8 +61,8 @@ function createGrid(size) {
     }    
 }
 
-
-button.addEventListener("click", () => {
+//Allows user to choose a size for the grid
+resizeButton.addEventListener("click", () => {
     let gridSize = prompt("Enter the number of squares per size (max 100): ", 16); 
     gridSize = parseInt(gridSize); 
 
@@ -49,6 +71,14 @@ button.addEventListener("click", () => {
     } else {
         createGrid(gridSize);
     }
+})
+
+//Reset the grid
+resetButton.addEventListener("click", () => {
+    const gridItems = container.querySelectorAll("div");
+    gridItems.forEach(gridItem => {
+        gridItem.style.backgroundColor = "transparent"; 
+    });
 })
 
 createGrid(16);
